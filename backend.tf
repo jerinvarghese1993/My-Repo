ec9cdcdd5bb8cda7705c885acfbc53318785163a
Jerin Varghese <163454926+jerinvarghese1993@users.noreply.github.com>
2024-09-06 16:29:05 -0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-sanjer-12345"  # Replace with your existing S3 bucket name
-    key    = "terraform.tfstate"        # The key to store the state file in the bucket
-    region = "us-east-1"               # The region where the bucket is located
-    dynamodb_table = "jerrystate-lock"
-    encrypt = true
-  }
-}
